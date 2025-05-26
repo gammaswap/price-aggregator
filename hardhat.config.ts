2d35242c66c7d790cf261a597ccb3e52e3932c0c
Daniel Alcarraz <dam417@nyu.edu>
2025-05-25 20:30:59 -0400

    remove task from hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 
